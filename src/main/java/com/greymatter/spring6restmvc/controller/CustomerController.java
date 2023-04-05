0586cf304b5eb10592f81f06527dfb2f6668e6d9
Jorge García <jcgarciamed@gmail.com>
2023-04-04 20:40:13 -0500

    Get Customers

---
+++ src/main/java/com/greymatter/spring6restmvc/controller/CustomerController.java
@@ -0,0 +1,31 @@
+package com.greymatter.spring6restmvc.controller;
+
+import com.greymatter.spring6restmvc.model.Customer;
+import com.greymatter.spring6restmvc.services.CustomerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.UUID;
+
+@RequestMapping("/api/v1/customer")
+@RequiredArgsConstructor
+@RestController
+public class CustomerController {
+
+    private final CustomerService customerService;
+
+    @GetMapping
+    public List<Customer> listAllCustomers(){
+        return customerService.getAllCustomers();
+    }
+
+    @GetMapping("/{customerId}")
+    public Customer getCustomerById(@PathVariable("customerId") UUID id){
+        return customerService.getCustomerById(id);
+    }
+
+}
---
+++ src/main/java/com/greymatter/spring6restmvc/services/CustomerService.java
@@ -0,0 +1,11 @@
+package com.greymatter.spring6restmvc.services;
+
+import com.greymatter.spring6restmvc.model.Customer;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface CustomerService {
+    Customer getCustomerById(UUID id);
+    List<Customer> getAllCustomers();
+}
