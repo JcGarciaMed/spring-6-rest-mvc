fb1a9c7b1d355c8fc7fe33fd1fbe6ca332ed796b
Jorge García <jcgarciamed@gmail.com>
2023-04-04 22:23:23 -0500

    POST

--- src/main/java/com/greymatter/spring6restmvc/controller/BeerController.java
+++ src/main/java/com/greymatter/spring6restmvc/controller/BeerController.java
@@ -4,10 +4,10 @@ import com.greymatter.spring6restmvc.model.Beer;
 import com.greymatter.spring6restmvc.services.BeerService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
@@ -28,6 +28,18 @@ public class BeerController {
     public Beer getBeerByUUID(@PathVariable("beerId") UUID beerId){
         log.debug("get beer by id - in controller");
         return beerService.getBeerById(beerId);
+    }
+
+    @PostMapping
+    //@RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity handlePost(@RequestBody Beer beer){
+
+        Beer savedBeer = beerService.saveNewBeer(beer);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Location", "/api/v1/beer/" + savedBeer.getId().toString());
+
+        return new ResponseEntity(headers, HttpStatus.CREATED);
     }
 
 
