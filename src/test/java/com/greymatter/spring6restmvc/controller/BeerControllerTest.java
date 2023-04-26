b7b28cabb7fe202bc7f63bce58dc71c453f44e16
Jorge García <jcgarciamed@gmail.com>
2023-04-25 21:19:28 -0500

    Beer Controller Binding Validation

--- src/test/java/com/greymatter/spring6restmvc/controller/BeerControllerTest.java
+++ src/test/java/com/greymatter/spring6restmvc/controller/BeerControllerTest.java
@@ -152,4 +152,17 @@ class BeerControllerTest {
                 .andExpect(jsonPath("$.id", is(testBeer.getId().toString())))
                 .andExpect(jsonPath("$.beerName", is(testBeer.getBeerName())));
     }
+    @Test
+    void testCreateBeerNullBeerName() throws Exception {
+
+        BeerDTO beerDTO = BeerDTO.builder().build();
+
+        given(beerService.saveNewBeer(any(BeerDTO.class))).willReturn(beerServiceImpl.listBeers().get(1));
+
+        mockMvc.perform(post(BeerController.BEER_PATH)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(beerDTO)))
+                .andExpect(status().isBadRequest());
+    }
 }
