51115ec0903de009ae710758531797f436343e6e
Jorge Garcia Medina <jcgarciamed@gmail.com>
2024-01-31 21:57:47 -0500

    Implementing CRUD Rest Services

--- src/main/java/com/greymatter/spring6restmvc/service/BeerService.java
+++ src/main/java/com/greymatter/spring6restmvc/service/BeerService.java
@@ -2,9 +2,19 @@ package com.greymatter.spring6restmvc.service;
 
 import com.greymatter.spring6restmvc.model.Beer;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface BeerService {
+    List<Beer> listBeers();
 
     Beer getBeerById(UUID id);
+
+    Beer saveNewBeer(Beer beer);
+
+    void updateBeerById(UUID beerId, Beer beer);
+
+    void deleteById(UUID beerId);
+
+    void patchBeerById(UUID beerId, Beer beer);
 }
