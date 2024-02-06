b48d0d52238f64a3e7005a40e620e4fa9e2c0809
Jorge Garcia Medina <jcgarciamed@gmail.com>
2024-02-05 20:03:19 -0500

    LOAD CSV FILES

---
+++ src/main/java/com/greymatter/spring6restmvc/service/BeerCsvService.java
@@ -0,0 +1,10 @@
+package com.greymatter.spring6restmvc.service;
+
+import com.greymatter.spring6restmvc.model.BeerCSVRecord;
+
+import java.io.File;
+import java.util.List;
+
+public interface BeerCsvService {
+    List<BeerCSVRecord> convertCSV(File csvFile);
+}
